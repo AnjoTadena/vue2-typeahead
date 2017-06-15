e1fba08599fc80a62903734b5d6775a05e13618e
Anjo Arinto Tadena <anjo@coderepubliq.com>
2017-06-15 11:31:36 +0800

    First commit.

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app">
+      <vue-typeahead 
+        placeholder="Search..."
+        displayKey="name"
+      >
+      </vue-typeahead>
+  </div>
+</template>
+
+<script>
+import Typeahead from './Typeahead.vue'
+
+export default {
+  name: 'app',
+  components: {
+    'vue-typeahead': Typeahead
+  },
+  data () {
+    return {
+      results: []
+    };
+  },
+  methods: {
+    filterItem (key) {
+      // this.results = []
+      // this.results = this.items.filter((item) => {
+      //   return item.indexOf(key) >= 0
+      // })
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+.typeahead li{
+  display: block;
+}
+</style>
---
+++ src/Typeahead.vue
@@ -0,0 +1,71 @@
+<template>
+	<div>
+		<input 
+			type="text"
+			:placeholder="placeholder"
+	        @keyup='up'
+		/>
+		 <ul 
+          slot="result"
+          class="typeahead"
+        >
+        	<li v-if="results.length == 0">No result foud</li>
+            <li v-else v-for="result in results">
+            	<a :href="'/link/' + result.id">
+            		{{ result[displayKey] }}
+            	</a>
+        	</li>
+        </ul>
+	</div>
+</template>
+<script>
+	export default {
+		props: {
+			placeholder: {
+				Type: 'String',
+				Default: 'Tyeahead Search'
+			},
+			displayKey: {
+				Type: 'String',
+				Required: true
+			}
+		},
+		data () {
+			return {
+				items: [
+					{
+						id: 1,
+						name: 'Anjo Tadena'
+					},{
+						id: 2,
+						name: 'Ranie Pabualan'
+					},{
+						id: 3,
+						name: 'Johnny Ramos'
+					},{
+						id: 4,
+						name: 'Apple Joe Pacomios'
+					},{
+						id: 5,
+						name: 'Eric Bray Quita'
+					},{
+						id: 6,
+						name: 'John Mark Liwat'
+					},{
+						id: 7,
+						name: 'Junivie Gorgonio'
+					},
+			     ],
+			     results: []
+			};
+		},
+		methods: {
+			up (e) {
+				this.results = [];
+				this.results = this.items.filter((item) => {
+					return item.name.indexOf(e.target.value) >= 0
+				})
+			}
+		}
+	}
+</script>
